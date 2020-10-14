3ada962c8fea88369ce00a7aa5cb4583dbb9e5dc
SelDanilEv <47692381+SelDanilEv@users.noreply.github.com>
2020-10-14 20:23:16 +0300

    feat: add lab 10

---
+++ Lab10/index.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const fs = require('fs');
+const WebSocket = require('ws');
+
+const PORT = 3000;
+const HOST = 'localhost';
+
+const app = express();
+const webSocketServer = new WebSocket.Server({port: 4000, host: HOST, path: '/wsserver'});
+const webSocketServerBroadcast = new WebSocket.Server({port: 5000, host: HOST, path: '/broadcast'});
+
+function loadHTML(request, response, source) {
+    new Promise((resolve, reject) => {
+        try {
+            let html = fs.readFileSync(source);
+            resolve(html);
+        } catch (err) {
+            reject(err);
+        }
+    }).then(html => {
+        response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+        response.end(html);
+    }).catch(err =>
+        console.log(err));
+}
+
+const server = app.listen(PORT, HOST, () => {
+    const URL = `http://${HOST}:${PORT}`;
+    console.log('Listening on ' + URL);
+}).on('error', (e) => {
+    console.log(`${URL} | error: ${e.code}`)
+});
+
+
+app.get('/start', (req, res) => {
+    loadHTML(req, res, "index.html");
+});
+
+let k = 0;
+let mess = 0;
+webSocketServer.on('connection', (ws) => {
+    console.log('WS connection');
+    ws.on('message', message => {
+        mess = message;
+        console.log(`client=> ${message}; server=> ${k}`);
+    });
+    let timer = setInterval(() => ws.send(`10-01-server: ${mess}->${k++}`), 5000);
+
+})
+    .on('error', (e) => {
+        console.log('WS server error ', e);
+    });
+
+let broadcastCounter =0;
+webSocketServerBroadcast.on('connection', (ws) => {
+    console.log('WS broadcast connection '+ ++broadcastCounter);
+    ws.on('message', message => {
+        webSocketServerBroadcast.clients.forEach((client) => {
+            console.log('Client message: ' + message);
+            if (client.readyState === WebSocket.OPEN)
+                client.send('Server -> ' + message)
+        });
+    });
+});
+
+webSocketServerBroadcast.on('open', () => {
+    let timer = ' ';
+    let k = 0;
+    timer = setInterval(() => webSocketServerBroadcast.send(`Client: -${k++}`));
+    webSocketServerBroadcast.on('message', (message) => {
+        console.log(`Received message => ${message}`);
+    });
+
+    setTimeout(() => {
+        clearInterval(timer);
+        webSocketServerBroadcast.close();
+    }, 25000);
+})
+    .on('error', (e) => {
+        console.log('WS server error ', e);
+    });
